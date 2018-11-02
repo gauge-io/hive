75408b4e87f957d256280b1e222194ab614ec1dd
Ashish Singh <ashish.me@outlook.com>
2018-11-02 14:18:51 +0530

    Basic version of custom WordTree working

--- assets/js/wordtree.js
+++ assets/js/wordtree.js
@@ -1,22 +1,30 @@
 /**
- * Word Tree Code
+ * Word Tree - Draws a word tree
+ * 
+ * @param {HTML element}  elSelectorTree  elSelector DOM element where the viz will be rendered
+ * @param {object}        oConfig         Configuration options for the Word Tree
+ * Properties of oConfig: {
+ *   type: [string] prefix, suffix, double
+ *   text: [string] Full text of the Tree
+ *   rootWord: [string] A word to be set as the Tree's root
+ * }
+ *
+ * @returns {object} Object with function named 'update' which accepts one parameter oConfig.
+ * The structure of this parameter is same as oConfig used during initialization.
+ * Call update() to update the Tree after instantiation.
  */
+function WordTree(elSelectorTree, oConfig) {
 
-function WordTree(oDispatch) {
-
-  // validate availability of Event Bus
-  if (!oDispatch) {
-    console.log('Error:', 'WordTree not initialised. No dispatch passed.');
+  // validate
+  if (!elSelectorTree) {
+    console.log('Error:', 'WordTree not initialised. No DOM element passed.');
     return false;
   }
-  
 
-  /**
-   * Dispatch event bus.
-   * This is how WordTree communicates with rest of the framework.
-   * Events supported: 
-   */
-  var dispatch = oDispatch,
+  // variable declaration
+  //   
+
+  var dispatch = d3.dispatch('libraryLoaded'),
 
   bIsLibraryLoaded = false,
 
@@ -32,16 +40,8 @@ function WordTree(oDispatch) {
 
     loadChartLibrary();
 
-    // initialize variables
-    // 
-    
-    oTreeOptions = {
-      wordtree: {
-        format: 'implicit',
-        type: 'suffix',
-        word: null
-      }
-    };
+    // Instantiate and draw the Tree
+    drawToDOM(elSelectorTree);
 
   }
 
@@ -73,59 +73,109 @@ function WordTree(oDispatch) {
    */
   function drawToDOM(elSelectorTree) {
 
-    if (bIsLibraryLoaded) {
-
-    }else{
-      dispatch.on('libraryLoaded.drawToDOM', function(){
-
-      });
-    }
-
     function _draw() {
 
-        // create instance of the word tree
-        oTree = new google.visualization.WordTree(elSelectorTree);
+      // prepare config and dataset for Word Tree
+      prepareConfig(oConfig);
 
-        drawTree(oConfig);
+      // create instance of the word tree
+      oTree = new google.visualization.WordTree(elSelectorTree);
+
+      drawTree();
         
+    }
+
+
+    if (bIsLibraryLoaded) {
+      
+      _draw();
+
+    }else{
+      
+      dispatch.on('libraryLoaded.drawToDOM', function(){
+        
+        _draw();
+
+      });
+
     }
     
   }
 
-  function prepareConfig() {
+  /**
+   * Prepare the configuration and dataset for Tree
+   * @param  {object} oParams Settings for word tree
+   * 
+   * Properties of oParams: {
+   *   type: [string] prefix, suffix, double
+   *   text: [string] Full text of the Tree
+   *   rootWord: [string] A word to be set as the Tree's root
+   * }
+   */
+  function prepareConfig(oParams) {
+
+    oParams = Object.assign({
+      type: 'suffix',
+      rootWord: null,
+      text: ''
+    }, oParams || {});
+    
+    // prepare default Tree config
+    oTreeOptions = {
+      wordtree: {
+        // We are only using implicit
+        format: 'implicit',
+        type: oParams.type,
+        word: oParams.rootWord
+      }
+    };
 
     // prepare dataset
-    oTreeData = google.visualization.arrayToDataTable(
-      [ ['Phrases'],
-        [sText]
-      ]
-    );
+    oTreeData = google.visualization.arrayToDataTable([ 
+      ['Phrases'],
+      [oParams.text]
+    ]);
     
   }
 
   /**
-   * Update the root word
-   * @param  {string} sRootWord A single word
+   * Update the Word Tree using oConfig parameters
+   * 
+   * @param  {object} oParams Settings for word tree
+   * 
+   * Properties of oParams: {
+   *   type: [string] prefix, suffix, double
+   *   text: [string] Full text of the Tree
+   *   rootWord: [string] A word to be set as the Tree's root
+   * }
    */
-  function updateRoot(sRootWord) {
+  function updateTree(oParams) {
+
+    prepareConfig(oParams);
+
+    drawTree();
     
   }
 
   /**
-   * Update the tree
-   * @param  {object} oParams Apply settings to the word tree
+   * Draw the tree
+   *
+   * It assumes that the dataset is availabel in variable oTreeData
+   * and options in oTreeOptions
    */
-  function drawTree(oConfig) {
+  function drawTree() {
 
-    // draw tree
-    oTree.draw(oConfig.data, oConfig.options);
+    // draw tree using prepared data and config variables
+    // 
+    oTree.draw(oTreeData, oTreeOptions);
     
   }
 
+  init();
 
   return {
 
-    draw: drawToDOM
+    update: updateTree
 
   }
 
